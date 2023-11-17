7921b11d78aed4299c25fa10b16c55c2a543c288
Upendra Sahu <upendrasahu1199@gmail.com>
2023-11-17 05:44:44 +0530

    [Upendra] Add:Implemented evend handler method with singup data

--- src/components/loginSignupPage/signupPage/Signup.jsx
+++ src/components/loginSignupPage/signupPage/Signup.jsx
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
 import "./Signup.css";
-import Email from "../emailTextBox/Email";
-import Password from "../passwordTextBox/Password";
 
 const Signup = (props) => {
+  //Initial State of all data
+  const [formValues, setFormValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    mobileNum: "",
+  });
+
+  //Event binding when data change it will add.
+  const handleU_NameChange = (e) => {
+    setFormValues({ ...formValues,  username: e.target.value});
+    console.log(formValues);
+  };
+  const handleEmailChange = (e) => {
+    setFormValues({...formValues, email: e.target.value});
+    console.log(formValues);
+  }
+  const handlePasswordChange = (e) => {
+    setFormValues({...formValues, password: e.target.value});
+    console.log(formValues);
+  }
+  const handleMobileN_Change = (e) => {
+    setFormValues({...formValues, mobileNum: e.target.value});
+    console.log(formValues);
+  }
   return (
     <div>
       {/* Accessing method as props for sending data to parent comonent (Base)*/}
@@ -16,20 +39,45 @@ const Signup = (props) => {
       <label for="inputName" id="fullNameLabel">
         Full Name
       </label>
-      <input type="text" id="inputName" className="inputName"></input>
+      <input
+        type="text"
+        id="inputName"
+        className="inputName"
+        value={formValues.username}
+        onChange={handleU_NameChange}
+      ></input>
 
       <label for="emailSignup" id="emailSignuplabel">
         Email id
       </label>
-      <input type="email" class="form-control" id="emailSignup"></input>
+      <input
+        type="email"
+        class="form-control"
+        id="emailSignup"
+        value={formValues.email}
+        onChange={handleEmailChange}
+      ></input>
 
       <label for="pwd" id="pwdLabel">
         Password:
       </label>
-      <input type="password" id="pwdSignup"></input>
+      <input
+        type="password"
+        id="pwdSignup"
+        value={formValues.password}
+        onChange={handlePasswordChange}
+      ></input>
 
-      <label for="mobSignup" id="mobLabelSignup">Mobile Number</label>
-      <input type="tel" id="mobSignup" name="mobile"/>
+      <label for="mobSignup" id="mobLabelSignup">
+        Mobile Number
+      </label>
+      <input
+        type="tel"
+        id="mobSignup"
+        name="mobile"
+        value={formValues.mobileNum}
+        onChange={handleMobileN_Change}
+      />
 
       <button type="button" id="signupButton">
         <div id="signupText">Signup</div>
