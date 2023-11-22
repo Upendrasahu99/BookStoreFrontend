77f2200d296d363dd5c7d8e090ca7004db810b77
Upendra Sahu <upendrasahu1199@gmail.com>
2023-11-22 15:39:42 +0530

    [Upendra] Add:Added Routing for Homepage and LoginSignup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Base from './components/loginSignupPage/basePage/Base';
-
+import HomPageBase from './components/homePage/homePageBase/HomePageBase';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Base/>
+      {/* {Wrap all the route in Routes} */}
+      <Routes>
+        {/* Give the path name and element which render on particular path  */}
+        <Route path='/' element={<HomPageBase/>}></Route>
+        <Route path='login/signup' element={<Base/>}></Route>
+      </Routes>
+      {/* <Base/>
+      <HomPageBase/> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from 'react-router-dom' //Import BrowserRouter for connect browser URL with our application
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter> {/* It allow us to use all react route feture in App Component tree */}
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
