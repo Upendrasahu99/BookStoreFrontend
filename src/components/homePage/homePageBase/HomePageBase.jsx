9a43232e5f686438b5d929fa178335f31f150b12
Upendra Sahu <upendrasahu1199@gmail.com>
2023-12-01 15:35:57 +0530

    [Upendra] Add: Added pagination for display all book

--- src/components/homePage/homePageBase/HomePageBase.jsx
+++ src/components/homePage/homePageBase/HomePageBase.jsx
@@ -5,7 +5,7 @@ import "./HomePageBase.css";
 import Display from "../showAllBook/Display";
 import { getAllBookService } from "../../../service/BookService";
 import BookDetail from "../bookDetail/BookDetail";
-
+import Pagination from "@mui/material/Pagination";
 
 const HomPageBase = () => {
   //Show profile container
@@ -17,6 +17,11 @@ const HomPageBase = () => {
   //Book data for single book
   const [bookDetail, setBookDetail] = useState();
 
+  //Set Book index for show
+  const [startIndex, setStartIndex] = useState(0);
+
+  //Total book in one page
+  const onePageBooks = 8;
 
   //Show profile container method
   const changeContainer = () => {
@@ -26,28 +31,43 @@ const HomPageBase = () => {
   //Set All Book
   const setAllNote = async () => {
     //Call getallBookService
+
     const response = await getAllBookService();
 
     //New Array for storing data which is comming rom getAllbookService
     let newArray = [];
     newArray = response.data.result;
 
-    //setBook Data 
+    //setBook Data
     setAllBook(newArray);
   };
 
-  //For Showing Single book detail page
-  const setBookDataForDisplay = (bookData) =>{
-    setBookDetail(bookData)
-    console.log(bookData)
-  }
+  //Set Data for one bookDetail
+  const setBookDataForDisplay = (bookData) => {
+    setBookDetail(bookData);
+    console.log(bookData);
+  };
 
+  //Sending all book data as prop in Display book component with detail
+  //Getting data back from all book for show Book Detail
+  let setDataOnAllBook = allBook.map((data) => (
+    <Display Book={data} SetBookData={setBookDataForDisplay} />
+  ));
+
+  //For Pagination set the book quantity
+  let bookShowInOnePage = setDataOnAllBook.slice(startIndex, (startIndex + onePageBooks));
+
+  //Set Page Num
+  let changePage = (event, paginationPageNum) => {
+    setStartIndex(paginationPageNum * onePageBooks - onePageBooks);
+    console.log(startIndex);
+  };
 
   return (
     <div className="homePageBase">
-      <div onClick={setAllNote}>   
+      <div onClick={setAllNote}>
         {/* Remove this when */}
-        <HomePageAppBar Change={changeContainer} Set ={setBookDataForDisplay}/>
+        <HomePageAppBar Change={changeContainer} Set={setBookDataForDisplay} />
       </div>
       <div className="proifileContainer">
         {showContainer ? <ProfileContainer /> : null}
@@ -56,34 +76,28 @@ const HomPageBase = () => {
         <div id="upperDiv">
           <p id="booksHeading">Books</p>
           {/* arrayLenght is equal to total book(item) */}
-          <p style={{paddingLeft:"10px"}}>({allBook.length} item)</p> 
+          <p style={{ paddingLeft: "10px" }}>({allBook.length} item)</p>
         </div>
 
-
-
-        {bookDetail != null ? 
-        //For showing Book Detail page
-        (
+        {bookDetail != null ? (
+          //For showing Book Detail page
           <div id="lowerDiv">
-            <BookDetail BookData={bookDetail}/>
+            <BookDetail BookData={bookDetail} />
           </div>
-        ) :
-        //For showing all book
-        (
-          <div id="lowerDiv">
-            {
-              //Sending all book data as prop in Display book component
-              
-              allBook.map((data) => (
-                <Display Book={data} SetBookData ={setBookDataForDisplay}/>
-              ))
-            }
-          </div>
+        ) : (
+          //For showing book in one page
+          <div id="lowerDiv">{bookShowInOnePage}</div>
         )}
 
-
-
-
+        {/* Pagination */}
+        <Pagination
+        //count is total page show in the pagination
+        // Math.cell which rounded upto nearest integer
+          count={Math.ceil((allBook.length)/onePageBooks)}
+          onChange={(e, paginationPageNum) => {
+            changePage(e, paginationPageNum);
+          }}
+        />
       </div>
     </div>
   );
